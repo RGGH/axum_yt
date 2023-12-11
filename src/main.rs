d97dccbc99fb87af74d9faf4ae92134eed791550
RGGH <pisscompany@gmail.com>
2023-12-11 15:36:27 +0000

    serve static files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use tower_http::services::ServeDir;
 use axum::{routing::get, Router};
 use tracing::info;
 
@@ -10,15 +11,18 @@ async fn main() {
     info!("starting server");
     info!("server running on port 3000");
 
-    let app = Router::new().route("/", get(root));
+    let service = ServeDir::new("assets");
+
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .nest_service("/assets",service);
+    // test assets ~ visit -> http://localhost:3000/assets
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 // basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn hello_world() -> &'static str {
+    "Hello, world!"
 }
-
-
