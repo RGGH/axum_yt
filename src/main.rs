20a6978816f7c7cfcfc6c986a16ac4378d8112a3
RGGH <pisscompany@gmail.com>
2023-12-11 11:15:11 +0000

    🟢update route for root

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,13 @@ async fn main() {
     use tokio::net::TcpListener;
 
     println!("Server running on http://0.0.0.0:3000");
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(root));
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
 }
