36a3b9de05e108ea7ee2495599268fe24872d531
RGGH <pisscompany@gmail.com>
2023-12-11 11:03:42 +0000

    add axum boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    use tokio::net::TcpListener;
+
+    println!("Server running on http://0.0.0.0:3000");
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
