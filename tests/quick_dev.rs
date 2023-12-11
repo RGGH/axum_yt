ac61ab19c0c7a3a182603ef8363f2fd3a040c806
RGGH <pisscompany@gmail.com>
2023-12-11 16:05:44 +0000

    add test for static files

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -1,9 +1,18 @@
 use anyhow::Result;
 
 #[tokio::test]
-async fn quick_dev() -> Result<()> {
+async fn quick_dev_root() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:3000/")?;
     hc.do_get("/").await?.print().await?;
 
     Ok(())
 }
+
+#[tokio::test]
+async fn quick_dev_assets() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:3000/")?;
+    hc.do_get("/assets").await?.print().await?;
+
+    Ok(())
+}
+
