ef34e09a98d7104f23f1706428b500dc414cc2f8
RGGH <pisscompany@gmail.com>
2023-12-11 12:03:16 +0000

    add logging info 🧐

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use axum::{routing::get, Router};
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
     use tokio::net::TcpListener;
 
-    println!("Server running on http://0.0.0.0:3000");
+    tracing_subscriber::fmt::init();
+    info!("starting server");
+
     let app = Router::new().route("/", get(root));
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -15,3 +18,5 @@ async fn main() {
 async fn root() -> &'static str {
     "Hello, World!"
 }
+
+
